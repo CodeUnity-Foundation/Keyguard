ef38ecd2d596ffe0420fdaec8e0b63bee293d4ed
rajpatelbot <rajpatel158g@gmail.com>
2024-02-17 14:59:26 +0530

    chore: Setup tailwind config

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,7 @@
+import base from '@vaultmaster/tailwind-config/tailwind.config';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  // ...base,
+  content: [...base.content],
+};
---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    // app content
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    '../../packages/ui/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
