8b3ae747e663ae9893afea6daf02890e48803297
rajpatelbot <rajpatel158g@gmail.com>
2024-02-19 00:23:28 +0530

    fix: Fix design issue and added icon

--- apps/web/src/app/auth/signup/page.tsx
+++ apps/web/src/app/auth/signup/page.tsx
@@ -3,25 +3,27 @@ import Image from 'next/image';
 import AuthLayout from '@web/components/auth';
 import SignupForm from '@web/components/auth/SignupForm';
 import SignupImage from '@web/assets/auth.svg';
+import logo from '@web/assets/icon.png';
 
 export default function Signup() {
   return (
     <AuthLayout
       leftPanelChild={
         <div className="flex flex-col w-full h-full justify-center items-center">
+          <Image src={logo} alt="VaultMaster" width={50} height={50} className="absolute top-10 left-10" />
           <Image src={SignupImage} alt="VaultMaster" width={550} height={550} />
         </div>
       }
       rightPanelChild={
         <div className="flex flex-col lg:w-[60%] md:w-3/4 w-[350px] h-full justify-center">
-          <div>
+          <div className="mb-5">
             <h1 className="font-medium lg:text-2xl text-xl">Welcome to VaultMaster</h1>
-            <p className="text-muted-300 lg:text-lg text-base font-medium mb-5">Sign up to get started</p>
+            <p className="text-muted-300 lg:text-lg text-base font-medium">Sign up to get started</p>
           </div>
           <SignupForm />
-          <p className="text-center text-muted-500 dark:text-muted-200 font-medium mt-10 text-sm text-muted-300">
+          <footer className="text-center text-muted-500 dark:text-muted-200 font-medium mt-10 text-sm">
             Powered by <span className="text-primary">CodeUnity Foundation</span>
-          </p>
+          </footer>
         </div>
       }
     />
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Head from 'next/head';
 import { Poppins } from 'next/font/google';
 import { cn } from '@vaultmaster/lib/cn';
 import '@vaultmaster/ui/styles/global.css';
@@ -14,12 +15,18 @@ export const metadata: Metadata = {
     template: '%s | VaultMaster',
     default: 'VaultMaster',
   },
+  icons: {
+    icon: '../assets/logo.png',
+  },
   description: 'Secure password manager for everyone',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/assets/icon.png" sizes="any" />
+      </Head>
       <body className={cn(`flex h-screen`)}>{children}</body>
     </html>
   );
