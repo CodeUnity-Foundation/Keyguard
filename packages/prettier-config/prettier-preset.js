eed40524a79949789a3352a3ec8dd65817d0b282
rajpatelbot <rajpatel158g@gmail.com>
2024-02-22 00:47:21 +0530

    chore: Configure prettier

---
+++ packages/prettier-config/prettier-preset.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  semi: true,
+  printWidth: 110,
+  arrowParens: "always",
+  importOrder: ["^@core/(.*)$", "<THIRD_PARTY_MODULES>", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
