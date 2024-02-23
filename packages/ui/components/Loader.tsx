9cd75da171c0afc093dda6b1e6662d631cbe15fd
rajpatelbot <rajpatel158g@gmail.com>
2024-02-23 07:06:11 +0530

    feat: Added common loader component

---
+++ packages/ui/components/Loader.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@vaultmaster/lib/cn";
+import { VariantProps, cva } from "class-variance-authority";
+
+const loaderVariants = cva("h-24 w-24 animate-spin rounded-full border-8", {
+  variants: {
+    variant: {
+      default: "border-primary border-solid border-t-transparent",
+      destructive: "border-destructive border-solid border-t-transparent",
+      secondary: "border border-input border-2 border-solid border-t-transparent",
+    },
+    size: {
+      default: "h-24 w-24",
+      sm: "h-6 w-6",
+      lg: "h-15 w-15",
+      xl: "h-28 w-28",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+interface LoaderProps extends VariantProps<typeof loaderVariants> {
+  className?: string;
+}
+
+export const Loader = ({ className, variant, size }: LoaderProps) => {
+  return (
+    <div className={cn(loaderVariants({ variant, size }), className)}>
+      <div className="h-full w-full"></div>
+    </div>
+  );
+};
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,3 @@
 export { Button, buttonVariants } from "./components/button";
 export { Input } from "./components/input";
+export { Loader } from "./components/Loader";
