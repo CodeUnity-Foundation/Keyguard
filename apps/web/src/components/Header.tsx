bc6f94d746ea206f100a19eb363c09c5ea3416cb
rajpatelbot <rajpatel158g@gmail.com>
2024-07-02 01:23:38 +0530

    style: Added menubar option, profile logo and dark-light mode switch .

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,9 +1,23 @@
-import { Input } from "@keyguard/ui";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  Input,
+} from "@keyguard/ui";
 import Logo from "@keyguard/web/assets/logo/symbol.png";
 import Image from "next/image";
 import Link from "next/link";
 import { BiSearch } from "react-icons/bi";
 import { RiMenu2Fill } from "react-icons/ri";
+
+import { ThemeSwitcher } from "./ThemeSwitcher";
 
 interface HeaderProps {
   sidebarOpen: boolean;
@@ -23,7 +37,7 @@ export default function Header({ sidebarOpen, setSidebarOpen }: HeaderProps) {
           </Link>
         </div>
 
-        <div className="w-full -mb-2 ml-10 lg:ml-0">
+        <div className="-mb-2 ml-10 flex w-full items-center justify-between lg:ml-0">
           <Input
             id="search"
             type="text"
@@ -32,6 +46,35 @@ export default function Header({ sidebarOpen, setSidebarOpen }: HeaderProps) {
             className="w-64 max-w-80 dark:bg-[#030614]"
             icon={<BiSearch className="text-primary" />}
           />
+
+          <div className="flex items-center justify-center gap-5">
+            <div className="mb-3">
+              <ThemeSwitcher />
+            </div>
+
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Avatar className="mb-3 cursor-pointer">
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator className="bg-muted-300 dark:bg-muted-400" />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <Link href="https://github.com/CodeUnity-Foundation/Keyguard" target="_blank">
+                    <DropdownMenuItem>GitHub</DropdownMenuItem>
+                  </Link>
+                  <DropdownMenuSeparator className="bg-muted-300 dark:bg-muted-400" />
+                  <DropdownMenuItem>Log out</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
         </div>
       </div>
     </header>
