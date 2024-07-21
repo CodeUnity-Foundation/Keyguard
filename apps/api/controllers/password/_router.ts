1ab250302a9f7e755fefaffd4980735a4daf61ad
rajpatelbot <rajpatel158g@gmail.com>
2024-07-22 01:40:18 +0530

    chore: Optimize code for procedures

--- apps/api/controllers/password/_router.ts
+++ apps/api/controllers/password/_router.ts
@@ -1,34 +1,30 @@
-import { checkValidAccessToken } from "../../middlewares/checkValidAccessToken";
-import { userAuthMiddleware } from "../../middlewares/userAuthMiddleware";
-import { publicProcedure, router } from "../../trpc/trpc";
+import { masterProtectedProcedure } from "../../procedures/authProcedure";
+import { router } from "../../trpc/trpc";
 
 export const passwordRouter = router({
-  getAllPasswords: publicProcedure
-    .use(userAuthMiddleware)
-    .use(checkValidAccessToken)
-    .query(async () => {
-      return {
-        status: 200,
-        success: true,
-        message: "Get all password successfully",
-        data: [
-          {
-            id: 1,
-            name: "password",
-            email: "X1wXv@example.com",
-            password: "password",
-            created_at: "2022-01-01 00:00:00",
-            updated_at: "2022-01-01 00:00:00",
-            deleted_at: null,
-            master_password: "password",
-            is_deleted: false,
-            is_visible: true,
-            is_pinned: true,
-            is_link_expired: false,
-            link: "https://keyguard.link/password",
-            link_expiry: "2022-01-01 00:00:00",
-          },
-        ],
-      };
-    }),
+  getAllPasswords: masterProtectedProcedure.query(async () => {
+    return {
+      status: 200,
+      success: true,
+      message: "Get all password successfully",
+      data: [
+        {
+          id: 1,
+          name: "password",
+          email: "X1wXv@example.com",
+          password: "password",
+          created_at: "2022-01-01 00:00:00",
+          updated_at: "2022-01-01 00:00:00",
+          deleted_at: null,
+          master_password: "password",
+          is_deleted: false,
+          is_visible: true,
+          is_pinned: true,
+          is_link_expired: false,
+          link: "https://keyguard.link/password",
+          link_expiry: "2022-01-01 00:00:00",
+        },
+      ],
+    };
+  }),
 });
--- apps/api/trpc/trpc.ts
+++ apps/api/trpc/trpc.ts
@@ -7,4 +7,5 @@ export const tRPCContext = initTRPC.context<typeof createContextInner>().create(
 export const router = tRPCContext.router;
 export const middleware = tRPCContext.middleware;
 export const mergeRouters = tRPCContext.mergeRouters;
-export const publicProcedure = tRPCContext.procedure;
+
+export const procedure = tRPCContext.procedure;
