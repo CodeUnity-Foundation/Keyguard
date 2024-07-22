47da68ea9b456041b2736f37f38e434bb472f8a7
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-07-22 22:03:00 +0530

    feat(web:ui): add favorites page

---
+++ apps/web/src/app/passwords/favorites/page.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "@keyguard/web/components/DefaultLayout";
+import FavoritesComp from "@keyguard/web/components/passwords/favorites";
+
+export default function Favorites() {
+  return (
+    <DefaultLayout>
+      <FavoritesComp />
+    </DefaultLayout>
+  );
+}
+
