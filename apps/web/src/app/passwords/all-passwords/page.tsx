fa7c43f9b5a70532c865445350ea936534a08f23
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-07-29 19:17:05 +0530

    refactor: rename file-casing in ui pkg and web

--- apps/web/src/app/passwords/all-passwords/page.tsx
+++ apps/web/src/app/passwords/all-passwords/page.tsx
@@ -1,5 +1,5 @@
 import DefaultLayout from "@keyguard/web/components/DefaultLayout";
-import AllPasswordComp from "./components/all-passwords";
+import AllPasswordComp from "./components/AllPasswords";
 
 export default function AllPasswords() {
   return (
